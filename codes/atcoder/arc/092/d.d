be3f3765aa475f63b5a267d8ef82a23658cb8668
te-sh <admire.japan@gmail.com>
2018-04-09 17:40:30 +0900

    add ABC #091 D

---
+++ codes/atcoder/arc/092/d.d
@@ -0,0 +1,44 @@
+// URL: https://beta.atcoder.jp/contests/abc091/tasks/arc092_b
+
+import std.algorithm, std.conv, std.range, std.stdio, std.string;
+
+void readV(T...)(ref T t){auto r=readln.splitter;foreach(ref v;t){v=r.front.to!(typeof(v));r.popFront;}}
+void readA(T)(size_t n,ref T t){t=new T(n);auto r=readln.splitter;foreach(ref v;t){v=r.front.to!(ElementType!T);r.popFront;}}
+void readM(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=readln.splitter;foreach(ref v;t){v[i]=r.front.to!(ElementType!(typeof(v)));r.popFront;}}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=readln.splitter;foreach(ref j;v.tupleof){j=r.front.to!(typeof(j));r.popFront;}}}
+
+void main()
+{
+  int n; readV(n);
+  int[] a; readA(n, a);
+  int[] b; readA(n, b);
+
+  auto r = 0;
+  foreach (k; 0..29) {
+    auto t = 2^^k;
+    auto am = a.map!(ai => ai%(t*2)).array;
+    auto bm = b.map!(bi => bi%(t*2)).array;
+    auto bs = bm.sort(), c = 0;
+    foreach (ai; am)
+      (c += bs.lowerBound(t*2-ai).length - bs.lowerBound(t-ai).length + bs.lowerBound(t*4-ai).length - bs.lowerBound(t*3-ai).length) %= 2;
+    if (c) r = r.bitSet(k);
+  }
+
+  writeln(r);
+}
+
+pragma(inline) {
+  pure bool bitTest(T)(T n, size_t i) { return (n & (T(1) << i)) != 0; }
+  pure T bitSet(T)(T n, size_t i) { return n | (T(1) << i); }
+  pure T bitReset(T)(T n, size_t i) { return n & ~(T(1) << i); }
+  pure T bitComp(T)(T n, size_t i) { return n ^ (T(1) << i); }
+
+  pure T bitSet(T)(T n, size_t s, size_t e) { return n | ((T(1) << e) - 1) & ~((T(1) << s) - 1); }
+  pure T bitReset(T)(T n, size_t s, size_t e) { return n & (~((T(1) << e) - 1) | ((T(1) << s) - 1)); }
+  pure T bitComp(T)(T n, size_t s, size_t e) { return n ^ ((T(1) << e) - 1) & ~((T(1) << s) - 1); }
+
+  import core.bitop;
+  pure int bsf(T)(T n) { return core.bitop.bsf(ulong(n)); }
+  pure int bsr(T)(T n) { return core.bitop.bsr(ulong(n)); }
+  pure int popcnt(T)(T n) { return core.bitop.popcnt(ulong(n)); }
+}
