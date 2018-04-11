c31aa4637cbee2d5f44a9f7d1ba7690897996a09
te-sh <admire.japan@gmail.com>
2018-04-11 17:38:01 +0900

    add ABC #083 D

---
+++ codes/atcoder/arc/088/d.d
@@ -0,0 +1,21 @@
+// URL: https://beta.atcoder.jp/contests/abc083/tasks/arc088_b
+
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
+
+void readV(T...)(ref T t){auto r=readln.splitter;foreach(ref v;t){v=r.front.to!(typeof(v));r.popFront;}}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=readln.splitter;foreach(ref v;t){v=r.front.to!T;r.popFront;}}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=readln.splitter;foreach(ref v;t){v[i]=r.front.to!(ElementType!(typeof(v)));r.popFront;}}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=readln.splitter;foreach(ref j;v.tupleof){j=r.front.to!(typeof(j));r.popFront;}}}
+
+version(unittest) {} else
+void main()
+{
+  string s; readV(s);
+
+  auto n = s.length.to!int, k = (n+1)/2+1;
+  for (; k <= n; ++k)
+    if (s[k-1] != s[n-k] || s[k-1] != s[k-2]) break;
+
+  writeln(k-1);
+}
