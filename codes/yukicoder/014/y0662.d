e9cb7215f594b536981ab09986aefe7b48b89272
te-sh <admire.japan@gmail.com>
2018-04-11 13:24:39 +0900

    apply new skelton for yukicoder codes

--- codes/yukicoder/014/y0662.d
+++ codes/yukicoder/014/y0662.d
@@ -1,20 +1,22 @@
 // URL: https://yukicoder.me/problems/no/662
 
-import std.algorithm, std.conv, std.range, std.stdio, std.string;
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
 
 void readV(T...)(ref T t){auto r=readln.splitter;foreach(ref v;t){v=r.front.to!(typeof(v));r.popFront;}}
-void readA(T)(size_t n,ref T t){t=new T(n);auto r=readln.splitter;foreach(ref v;t){v=r.front.to!(ElementType!T);r.popFront;}}
-void readM(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=readln.splitter;foreach(ref v;t){v[i]=r.front.to!(ElementType!(typeof(v)));r.popFront;}}}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=readln.splitter;foreach(ref v;t){v=r.front.to!T;r.popFront;}}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref ti;t)readA(c,ti);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=readln.splitter;foreach(ref v;t){v[i]=r.front.to!(ElementType!(typeof(v)));r.popFront;}}}
 void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=readln.splitter;foreach(ref j;v.tupleof){j=r.front.to!(typeof(j));r.popFront;}}}
 
+version(unittest) {} else
 void main()
 {
   int m = 5, k = 3;
-  string[] s; int[] c; readM(m, s, c);
+  string[] s; int[] c; readC(m, s, c);
   auto n = new long[](k), a = new string[][](k);
   foreach (i; 0..k) {
     readV(n[i]);
-    readM(n[i], a[i]);
+    readC(n[i], a[i]);
   }
 
   auto u = new long[](m);
