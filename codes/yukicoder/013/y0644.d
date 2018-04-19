0bf19ada45ed7a74bd2d1c85dd3892c1811551dc
te-sh <admire.japan@gmail.com>
2018-04-19 17:24:54 +0900

    add yukicoder No.644

---
+++ codes/yukicoder/013/y0644.d
@@ -0,0 +1,135 @@
+// URL: https://yukicoder.me/problems/no/644
+
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
+import std.bitmanip;
+
+auto rdsp(){return readln.splitter;}
+void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
+void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
+void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
+
+const mod = 10^^9+7;
+alias mint = FactorRing!mod;
+
+version(unittest) {} else
+void main()
+{
+  int n, m; readV(n, m);
+
+  auto p = primes(n);
+
+  auto factors(int n)
+  {
+    int[] f;
+    while (n > 1) {
+      auto fi = factor(n, p);
+      if (f.empty || f[$-1] != fi) f ~= fi;
+      n /= fi;
+    }
+    return f;
+  }
+
+  auto q = n/m, ans = mint(0);
+  foreach (i; 1..q+1) {
+    auto f = factors(i), nf = f.length.to!int, r = q;
+    if (i == 1) --r;
+    foreach (j; 1..1<<nf) {
+      auto fp = f.indexed(j.bitsSet).fold!"a*b";
+      r += (-1)^^j.popcnt * (q/fp);
+    }
+    ans += r;
+  }
+
+  foreach (i; 1..n-1) ans *= i;
+  writeln(ans);
+}
+
+pragma(inline) {
+  pure bool bitTest(T)(T n, size_t i) { return (n & (T(1) << i)) != 0; }
+  pure T bitSet(T)(T n, size_t i) { return n | (T(1) << i); }
+  pure T bitReset(T)(T n, size_t i) { return n & ~(T(1) << i); }
+  pure T bitComp(T)(T n, size_t i) { return n ^ (T(1) << i); }
+
+  pure T bitSet(T)(T n, size_t s, size_t e) { return n | ((T(1) << e) - 1) & ~((T(1) << s) - 1); }
+  pure T bitReset(T)(T n, size_t s, size_t e) { return n & (~((T(1) << e) - 1) | ((T(1) << s) - 1)); }
+  pure T bitComp(T)(T n, size_t s, size_t e) { return n ^ ((T(1) << e) - 1) & ~((T(1) << s) - 1); }
+
+  import core.bitop;
+  pure int bsf(T)(T n) { return core.bitop.bsf(ulong(n)); }
+  pure int bsr(T)(T n) { return core.bitop.bsr(ulong(n)); }
+  pure int popcnt(T)(T n) { return core.bitop.popcnt(ulong(n)); }
+}
+
+pure T[] primes(T)(T n)
+{
+  import std.algorithm, std.bitmanip, std.conv, std.range;
+
+  auto sieve = BitArray();
+  sieve.length((n+1)/2);
+  sieve = ~sieve;
+
+  foreach (p; 1..((nsqrt(n)-1)/2+1))
+    if (sieve[p])
+      for (auto q = p*3+1; q < (n+1)/2; q += p*2+1)
+        sieve[q] = false;
+
+  auto r = sieve.bitsSet.map!(to!T).map!("a*2+1").array;
+  r[0] = 2;
+
+  return r;
+}
+
+pure T factor(T)(T n, const T[] p)
+{
+  auto ma = nsqrt(n)+1;
+  foreach (pi; p)
+    if (pi > ma) return n;
+    else if (n%pi == 0) return pi;
+  return n;
+}
+
+pure T nsqrt(T)(T n)
+{
+  import std.algorithm, std.conv, std.range, core.bitop;
+  if (n <= 1) return n;
+  T m = T(1) << (n.bsr/2+1);
+  return iota(1, m).map!"a*a".assumeSorted!"a <= b".lowerBound(n).length.to!T;
+}
+
+struct FactorRing(int m, bool pos = false)
+{
+  version(BigEndian) union { long vl; struct { int vi2; int vi; } } else union { long vl; int vi; }
+  alias FR = FactorRing!(m, pos);
+  @property static init() { return FR(0); }
+  @property int value() { return vi; }
+  @property void value(int v) { vi = mod(v); }
+  alias value this;
+
+  this(int v) { vi = v; }
+  this(int v, bool runMod) { vi = runMod ? mod(v) : v; }
+  this(long v) { vi = mod(v); }
+
+  ref auto opAssign(int v) { vi = v; return this; }
+
+  pure auto mod(int v) const { static if (pos) return v%m; else return (v%m+m)%m; }
+  pure auto mod(long v) const { static if (pos) return cast(int)(v%m); else return cast(int)((v%m+m)%m); }
+
+  static if (!pos) pure ref auto opUnary(string op: "-")() { return FR(mod(-vi)); }
+
+  static if (m < int.max / 2) {
+    pure ref auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FR(mod(mixin("vi"~op~"r"))); }
+    ref auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vi"~op~"r")); return this; }
+  } else {
+    pure ref auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FR(mod(mixin("vl"~op~"r"))); }
+    ref auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vl"~op~"r")); return this; }
+  }
+  pure ref auto opBinary(string op: "*")(int r) { return FR(mod(vl*r)); }
+  ref auto opOpAssign(string op: "*")(int r) { vi = mod(vl*r); return this; }
+
+  pure ref auto opBinary(string op)(ref FR r) if (op == "+" || op == "-" || op == "*") { return opBinary!op(r.vi); }
+  ref auto opOpAssign(string op)(ref FR r) if (op == "+" || op == "-" || op == "*") { return opOpAssign!op(r.vi); }
+}
