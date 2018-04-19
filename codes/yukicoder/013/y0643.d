b210acf22a5130d22ffdb140dbfff426691c7cb0
te-sh <admire.japan@gmail.com>
2018-04-19 16:55:31 +0900

    add yukicoder No.642,643

---
+++ codes/yukicoder/013/y0643.d
@@ -0,0 +1,29 @@
+// URL: https://yukicoder.me/problems/no/643
+
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
+
+auto rdsp(){return readln.splitter;}
+void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
+void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
+void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
+
+version(unittest) {} else
+void main()
+{
+  int x, y; readV(x, y);
+
+  if (x != 0 && y != 0 && x.abs != y.abs) {
+    writeln(-1);
+    return;
+  }
+
+  auto a = [[5, 6, 7], [4, 1, 0], [3, 2, 1]];
+  auto d = [1, 0, 2, 3, 1, 0, 2, 3];
+  writeln(d[a[y.sgn+1][x.sgn+1]]);
+}
+
+auto sgn(T)(T n) { return n > 0 ? 1 : n < 0 ? -1 : 0; }
