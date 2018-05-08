a5ea919fe540566968f390c51eaf5aa03996545e
te-sh <admire.japan@gmail.com>
2018-05-08 16:13:13 +0900

    add yukicoder No.681

---
+++ codes/yukicoder/014/y0681.d
@@ -0,0 +1,131 @@
+// URL: https://yukicoder.me/problems/no/681
+
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
+
+auto rdsp(){return readln.splitter;}
+void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
+void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
+void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
+
+const mod = 10^^9+7;
+alias mint = FactorRing!mod;
+
+version(unittest) {} else
+void main()
+{
+  int n; readV(n);
+  int b, d; readV(b, d);
+
+  auto t = (mint(d+1).repeatedSquare(b)-1)*(d+1)/d-b;
+
+  long[] h = [0, d], s = [0, d];
+  foreach (i; 2..31) {
+    auto hi = h[i-1] * (d+1) + d;
+    if (hi > n) break;
+    h ~= hi;
+
+    auto si = s[i-1] * (d+1) + i*d;
+    s ~= si;
+  }
+
+  while (n > 0) {
+    auto i = h.filter!(hi => hi <= n).walkLength.to!int - 1;
+    if (i == 0) {
+      t -= n;
+      n = 0;
+    } else {
+      t -= mint(s[i]);
+      n -= h[i];
+      if (n > 0) {
+        t -= i+1;
+        --n;
+      }
+    }
+  }
+
+  writeln(t);
+}
+
+pure T repeatedSquare(alias pred = "a * b", T, U)(T a, U n)
+{
+  return repeatedSquare!(pred, T, U)(a, n, T(1));
+}
+
+pure T repeatedSquare(alias pred = "a * b", T, U)(T a, U n, T init)
+{
+  import std.functional;
+  alias predFun = binaryFun!pred;
+
+  if (n == 0) return init;
+
+  auto r = init;
+  while (n > 0) {
+    if (n&1) r = predFun(r, a);
+    a = predFun(a, a);
+    n >>= 1;
+  }
+
+  return r;
+}
+
+struct FactorRing(int m, bool pos = false)
+{
+  version(BigEndian) union { long vl; struct { int vi2; int vi; } } else union { long vl; int vi; }
+  alias FR = FactorRing!(m, pos);
+  @property static init() { return FR(0); }
+  @property int value() { return vi; }
+  @property void value(int v) { vi = mod(v); }
+  alias value this;
+
+  this(int v) { vi = v; }
+  this(int v, bool runMod) { vi = runMod ? mod(v) : v; }
+  this(long v) { vi = mod(v); }
+
+  ref auto opAssign(int v) { vi = v; return this; }
+
+  pure auto mod(int v) const { static if (pos) return v%m; else return (v%m+m)%m; }
+  pure auto mod(long v) const { static if (pos) return cast(int)(v%m); else return cast(int)((v%m+m)%m); }
+
+  static if (!pos) pure ref auto opUnary(string op: "-")() { return FR(mod(-vi)); }
+
+  static if (m < int.max / 2) {
+    pure ref auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FR(mod(mixin("vi"~op~"r"))); }
+    ref auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vi"~op~"r")); return this; }
+  } else {
+    pure ref auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FR(mod(mixin("vl"~op~"r"))); }
+    ref auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vl"~op~"r")); return this; }
+  }
+  pure ref auto opBinary(string op: "*")(int r) { return FR(mod(vl*r)); }
+  ref auto opOpAssign(string op: "*")(int r) { vi = mod(vl*r); return this; }
+
+  pure ref auto opBinary(string op)(ref FR r) if (op == "+" || op == "-" || op == "*") { return opBinary!op(r.vi); }
+  ref auto opOpAssign(string op)(ref FR r) if (op == "+" || op == "-" || op == "*") { return opOpAssign!op(r.vi); }
+
+  pure auto opBinary(string op: "/")(FR r) { return FR(mod(vl*r.inv.vi)); }
+  pure auto opBinary(string op: "/")(int r) { return opBinary!op(FR(r)); }
+  ref auto opOpAssign(string op: "/")(ref FR r) { vi = mod(vl*r.inv.vi); return this; }
+  ref auto opOpAssign(string op: "/")(int r) { return opOpAssign!op(FR(r)); }
+
+  pure auto inv()
+  {
+    int x = vi, a, b;
+    exEuclid(x, m, a, b);
+    return FR(mod(a));
+  }
+}
+
+pure T exEuclid(T)(T a, T b, ref T x, ref T y)
+{
+  auto g = a;
+  x = 1;
+  y = 0;
+  if (b) {
+    g = exEuclid(b, a%b, y, x);
+    y -= a/b*x;
+  }
+  return g;
+}
