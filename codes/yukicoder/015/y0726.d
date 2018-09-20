0acaeec9ffa297110c6919c5c633d33672e22a5a
te-sh <admire.japan@gmail.com>
2018-09-20 12:13:21 +0900

    add yukicoder No.725,726,727,728

---
+++ codes/yukicoder/015/y0726.d
@@ -0,0 +1,82 @@
+// URL: https://yukicoder.me/problems/no/726
+
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
+
+auto rdsp(){return readln.splitter;}
+void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
+void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
+void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
+void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
+
+version(unittest) {} else
+void main()
+{
+  auto p = primes(10^^5);
+
+  int y, x; readV(y, x);
+
+  if (y == 2 || x == 2) {
+    writeln("Second");
+    return;
+  }
+
+  auto isPrime(int n)
+  {
+    if (n == 1) return false;
+
+    foreach (pi; p) {
+      if (pi > n.nsqrt) break;
+      if (n%pi == 0) return false;
+    }
+
+    return true;
+  }
+
+  auto nextPrime(int n)
+  {
+    ++n;
+    for (;; ++n) if (isPrime(n)) return n;
+    assert(0);
+  }
+
+  auto yp = isPrime(y), xp = isPrime(x);
+  if (yp && xp) {
+    writeln("Second");
+    return;
+  }
+
+  auto ny = nextPrime(y), nx = nextPrime(x);
+  auto t = (ny-y)+(nx-x);
+  writeln(t%2 == 0 ? "Second" : "First");
+}
+
+pure T[] primes(T)(T n)
+{
+  import std.algorithm, std.bitmanip, std.conv, std.range;
+
+  auto sieve = BitArray();
+  sieve.length((n+1)/2);
+  sieve = ~sieve;
+
+  foreach (p; 1..((nsqrt(n)-1)/2+1))
+    if (sieve[p])
+      for (auto q = p*3+1; q < (n+1)/2; q += p*2+1)
+        sieve[q] = false;
+
+  auto r = sieve.bitsSet.map!(to!T).map!("a*2+1").array;
+  r[0] = 2;
+
+  return r;
+}
+
+pure T nsqrt(T)(T n)
+{
+  import std.algorithm, std.conv, std.range, core.bitop;
+  if (n <= 1) return n;
+  T m = T(1) << (n.bsr/2+1);
+  return iota(1, m).map!"a*a".assumeSorted!"a <= b".lowerBound(n).length.to!T;
+}
---
+++ codes/yukicoder/015/y0728.d
@@ -0,0 +1,85 @@
+// URL: https://yukicoder.me/problems/no/728
+
+import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
+
+auto rdsp(){return readln.splitter;}
+void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
+void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
+void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
+void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
+void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
+void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
+void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
+
+version(unittest) {} else
+void main()
+{
+  int n; readV(n);
+  int[] a; readA(n, a);
+  int[] l, r; readC(n, l, r);
+
+  struct B { int x, i, t; }
+  auto b = new B[](n*2);
+  foreach (i; 0..n) {
+    b[i] = B(a[i], i, 0);
+    b[i+n] = B(a[i]-l[i], i, 1);
+  }
+  b.sort!"a.x == b.x ? a.t > b.t : a.x < b.x";
+
+  auto as = a.assumeSorted, ft = new FenwickTree!int(n), ans = 0L;
+
+  foreach (bi; b) {
+    auto i = bi.i;
+    switch (bi.t) {
+    case 0:
+      auto k = as.lowerBound(a[i]+r[i]+1).length.to!int;
+      ans += ft[i+1..k];
+      break;
+    case 1:
+      ++ft[i];
+      break;
+    default:
+      assert(0);
+    }
+  }
+
+  writeln(ans);
+}
+
+class FenwickTree(T)
+{
+  const size_t n;
+  T[] buf;
+
+  this(size_t n)
+  {
+    this.n = n;
+    this.buf = new T[](n+1);
+  }
+
+  void opIndexOpAssign(string op)(T val, size_t i) if (op == "+" || op == "-")
+  {
+    ++i;
+    for (; i <= n; i += i & -i) mixin("buf[i] " ~ op ~ "= val;");
+  }
+
+  void opIndexUnary(string op)(size_t i) if (op == "++" || op == "--")
+  {
+    ++i;
+    for (; i <= n; i += i & -i) mixin("buf[i]" ~ op ~ ";");
+  }
+
+  pure T opSlice(size_t r, size_t l) { return get(l) - get(r); }
+  pure T opIndex(size_t i) { return opSlice(i, i+1); }
+  pure size_t opDollar() { return n; }
+
+private:
+
+  pure T get(size_t i)
+  {
+    auto s = T(0);
+    for (; i > 0; i -= i & -i) s += buf[i];
+    return s;
+  }
+}
