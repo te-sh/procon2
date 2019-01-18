e18c7545472c227f8e29b9371b7e65621329032f
te-sh <admire.japan@gmail.com>
2019-01-18 11:57:07 +0900

    implement judge

--- judge/runoj.d
+++ judge/runoj.d
@@ -1,31 +1,49 @@
-import std.stdio, std.file, std.process;
+import std.stdio, std.file, std.process, std.typecons;
 import std.string, std.regex, std.json, std.conv;
 
 void main()
 {
-  auto config = readText("config.json").parseJSON;
+  auto configJson = readText("config.json").parseJSON;
   auto prevUrl = "";
 
   while (true) {
     auto line = readln.split;
     auto path = line[0] ~ line[2], site = getSite(path);
-    if (site !in config) continue;
+    if (site !in configJson) continue;
+
+    writeln("==================== ", path);
+    auto config = parseConfig(configJson[site]);
+    auto env = config.env, compileOption = config.compileOption;
 
     auto url = getUrl(path);
     if (url.empty) continue;
 
     if (url != prevUrl) {
-      downloadTest(url);
+      if (downloadTest(url)) continue;
       prevUrl = url;
     }
 
-    compile(path, config[site]);
+    if (compile(path, env, compileOption)) continue;
+
+    runTest(env);
   }
 }
 
 auto getSite(string path)
 {
   return path.split('/')[2];
+}
+
+const rootDir = "/root/dlang/";
+
+auto parseConfig(JSONValue config)
+{
+  auto dlangDir = rootDir ~ config["dlang"].str;
+  auto binDir = config["binDir"].str, libDir = config["libDir"].str;
+  auto path = dlangDir ~ binDir ~ ":" ~ environment.get("PATH"), libPath = dlangDir ~ libDir;
+  auto env = ["PATH": path, "LIBRARY_PATH": libPath, "LD_LIBRARY_PATH": libPath];
+  auto compileOption = config["compileOption"].str.split;
+  return tuple!("env", "compileOption")(env, compileOption);
 }
 
 auto getUrl(string path)
@@ -40,30 +58,26 @@ auto getUrl(string path)
 
 auto downloadTest(string url)
 {
+  writeln("---------- Download Test");
   if (exists("test")) rmdirRecurse("test");
-  writeln("=============== Download Test");
-  runProcess(["oj", "download", url]);
+  return runProcess(["oj", "download", url]);
 }
 
-const rootDir = "/root/dlang";
-
-auto compile(string path, JSONValue config)
+auto compile(string path, string[string] env, string[] compileOption)
 {
-  writeln("=============== Compile");
+  writeln("---------- Compile");
+  return runProcess(["dmd"] ~ compileOption ~ [path], env);
+}
 
-  auto env = ["PATH": rootDir ~ config["compilerDir"].str,
-              "LIBRARY_PATH": rootDir ~ config["libraryDir"].str,
-              "LD_LIBRARY_PATH": rootDir ~ config["libraryDir"].str];
-  auto opt = config["compileOption"].str.split;
-
-  runProcess(["dmd"] ~ opt ~ ["-of=a.out", path], env);
+auto runTest(string[string] env)
+{
+  writeln("---------- Run Test");
+  return runProcess(["oj", "test"], env);
 }
 
 auto runProcess(string[] cmd, string[string] env = null)
 {
-  auto pipe = pipeProcess(cmd, Redirect.stdout | Redirect.stderrToStdout, env);
-  while (true) {
-    foreach (line; pipe.stdout.byLine) writeln(line);
-    if (tryWait(pipe.pid).terminated) break;
-  }
+  writeln("> ", cmd.join(" "));
+  auto pid = spawnShell(cmd.join(" "), env);
+  return wait(pid);
 }
