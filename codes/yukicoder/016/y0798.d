e8b0cf22f2592283beb48dd5a27cec569b359159
te-sh <admire.japan@gmail.com>
2020-01-09 04:42:29 +0900

    add yukicoder No.798

---
+++ codes/yukicoder/016/y0798.d
@@ -0,0 +1,120 @@
+// URL: https://yukicoder.me/problems/no/798
+
+import std.algorithm, std.container, std.math, std.range, std.typecons, std.string;
+
+version(unittest) {} else
+void main()
+{
+  int n; io.getV(n);
+  int[] a, b; io.getC(n, a, b);
+
+  struct M { int a, b; }
+  auto m = new M[](n);
+  foreach (ref mi, ai, bi; lockstep(m, a, b)) mi = M(ai, bi);
+  m.sort!"a.b > b.b";
+
+  auto dp = new long[][](n+1, n/3+1);
+  foreach (ref dpi; dp) dpi[] = 10L^^18;
+  dp[0][0] = 0;
+
+  foreach (i; 0..n)
+    foreach (j; 0..n/3+1) {
+      if (i-j >= 0)
+	dp[i+1][j] = min(dp[i+1][j], dp[i][j] + m[i].a + (i-j) * m[i].b);
+      if (j >= 1)
+	dp[i+1][j] = min(dp[i+1][j], dp[i][j-1]);
+    }
+
+  io.put(dp[$-1][$-1]);
+}
+
+auto io = IO();
+
+struct IO
+{
+  import std.algorithm, std.conv, std.format, std.meta, std.range, std.stdio, std.traits;
+
+  dchar[] buf;
+  auto sp = (new dchar[](0)).splitter;
+  int precision = 10;
+  string delimiter = " ";
+
+  void nextLine()
+  {
+    stdin.readln(buf);
+    sp = buf.splitter;
+  }
+
+  auto get(T)(ref T v)
+  {
+    if (sp.empty) nextLine();
+    v = sp.front.to!T;
+    sp.popFront();
+  }
+
+  auto getV(T...)(ref T v)
+  {
+    foreach (ref w; v) get(w);
+  }
+
+  auto getA(T)(size_t n, ref T v)
+  if (hasAssignableElements!T)
+  {
+    v = new T(n);
+    foreach (ref w; v) get(w);
+  }
+
+  auto getC(T...)(size_t n, ref T v)
+  if (allSatisfy!(hasAssignableElements, T))
+  {
+    foreach (ref w; v)
+      w = new typeof(w)(n);
+    foreach (i; 0..n)
+      foreach (ref w; v) get(w[i]);
+  }
+
+  auto getM(T)(size_t r, size_t c, ref T v)
+  if (hasAssignableElements!T && hasAssignableElements!(ElementType!T))
+  {
+    v = new T(r);
+    foreach (ref w; v) getA(c, w);
+  }
+
+  auto rangePop(R)(ref R r)
+  {
+    r.popFront();
+    if (!r.empty) write(delimiter);
+  }
+
+  auto putA(T)(T v)
+  {
+    static if (isInputRange!T && !isSomeString!T)
+      for (auto w = v; !w.empty; rangePop(w)) putA(w.front);
+    else if (isFloatingPoint!T)
+      writef(format("%%.%df", precision), v);
+    else
+      write(v);
+  }
+
+  auto put(T...)(T v)
+  {
+    foreach (i, w; v) {
+      putA(w);
+      if (i < v.length-1) write(delimiter);
+    }
+    writeln;
+  }
+
+  auto putB(S, T)(bool c, S t, T f)
+  {
+    if (c)
+      put(t);
+    else
+      put(f);
+  }
+
+  auto dbg(T...)(T v)
+  {
+    stderr.writeln(v);
+  }
+}
