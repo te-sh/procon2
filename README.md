50bf5f83af88e03412703379246090e860bc68c5
te-sh <admire.japan@gmail.com>
2020-01-09 15:34:10 +0900

    move config.json to docker-compose.yml
    a# Please enter the commit message for your changes. Lines starting

---
+++ README.md
@@ -0,0 +1,7 @@
+## Caution
+
+Now this project doesn't work fine because of [issue 19116](https://issues.dlang.org/show_bug.cgi?id=19116) of DMD.
+
+(I guess it turns to work fine if you change to use python:3.7.6-stretch for Docker base.)
+
+I'm looking forward to being fixed the issue.
--- judge/runoj.d
+++ judge/runoj.d
@@ -1,51 +1,37 @@
-import std.stdio, std.file, std.process, std.typecons;
-import std.string, std.regex, std.json, std.conv;
+import std.algorithm, std.conv, std.file, std.process;
+import std.regex, std.stdio, std.string, std.typecons, std.uni;
 
 void main()
 {
-  auto configJson = readText("config.json").parseJSON;
   auto prevUrl = "";
+  auto sites = environment.get("SITES").split;
 
   while (true) {
     auto path = readln.chomp;
     if (!exists(path) || isDir(path)) continue;
 
     auto site = getSite(path);
-    if (site !in configJson) continue;
+    if (!sites.canFind(site)) continue;
 
     writeln("==================== ", path);
-    auto config = parseConfig(configJson[site]);
-    auto env = config.env, compileOption = config.compileOption;
 
     auto url = getUrl(path);
     if (url.empty) continue;
 
     if (url != prevUrl) {
-      if (downloadTest(url)) continue;
+      if (runProcess(["./oj-download", url])) continue;
       prevUrl = url;
     }
 
-    if (compile(path, env, compileOption)) continue;
-
-    runTest(env);
+    auto env = ["SITE": site.toUpper];
+    if (runProcess(["./dmd-compile", path], env)) continue;
+    runProcess(["./oj-test"], env);
   }
 }
 
 auto getSite(string path)
 {
   return path.split('/')[2];
-}
-
-const rootDir = "/root/dlang/";
-
-auto parseConfig(JSONValue config)
-{
-  auto dlangDir = rootDir ~ config["dlang"].str;
-  auto binDir = config["binDir"].str, libDir = config["libDir"].str;
-  auto path = dlangDir ~ binDir ~ ":" ~ environment.get("PATH"), libPath = dlangDir ~ libDir;
-  auto env = ["PATH": path, "LIBRARY_PATH": libPath, "LD_LIBRARY_PATH": libPath];
-  auto compileOption = config["compileOption"].str.split;
-  return tuple!("env", "compileOption")(env, compileOption);
 }
 
 auto getUrl(string path)
@@ -58,28 +44,8 @@ auto getUrl(string path)
   return "";
 }
 
-auto downloadTest(string url)
-{
-  writeln("---------- Download Test");
-  if (exists("test")) rmdirRecurse("test");
-  return runProcess(["oj", "download", url]);
-}
-
-auto compile(string path, string[string] env, string[] compileOption)
-{
-  writeln("---------- Compile");
-  return runProcess(["dmd"] ~ compileOption ~ [path], env);
-}
-
-auto runTest(string[string] env)
-{
-  writeln("---------- Run Test");
-  return runProcess(["oj", "test"], env);
-}
-
 auto runProcess(string[] cmd, string[string] env = null)
 {
-  writeln("> ", cmd.join(" "));
   auto pid = spawnShell(cmd.join(" "), env);
   return wait(pid);
 }
