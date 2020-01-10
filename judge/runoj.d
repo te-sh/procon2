9c19bb5071ab421e9180260ea038fb522ee4a88b
te-sh <admire.japan@gmail.com>
2020-01-10 20:49:00 +0900

    handle multiple notification of code

--- judge/runoj.d
+++ judge/runoj.d
@@ -3,45 +3,89 @@ import std.regex, std.stdio, std.string, std.typecons, std.uni;
 
 void main()
 {
-  auto prevUrl = "";
-  auto sites = environment.get("SITES").split;
+  auto path = Path("SITES"), url = Url();
 
   while (true) {
-    auto path = readln.chomp;
-    if (!exists(path) || isDir(path)) continue;
-
-    auto site = getSite(path);
-    if (!sites.canFind(site)) continue;
+    path.setFromINotify(readln.chomp);
+    if (!path.valid || !path.changed) continue;
 
     writeln("==================== ", path);
 
-    auto url = getUrl(path);
-    if (url.empty) continue;
+    url.setFromFile(path);
+    if (!url.valid) continue;
 
-    if (url != prevUrl) {
+    if (url.changed)
       if (runProcess(["./oj-download", url])) continue;
-      prevUrl = url;
-    }
 
-    auto env = ["SITE": site.toUpper];
+    auto env = ["SITE": path.site.toUpper];
     if (runProcess(["./dmd-compile", path], env)) continue;
     runProcess(["./oj-test"], env);
   }
 }
 
-auto getSite(string path)
+struct Path
 {
-  return path.split('/')[2];
+  string path, prevPath, site;
+  long time, prevTime;
+  bool valid;
+  string[] sites;
+  alias path this;
+
+  auto this(string envName) { sites = environment.get(envName).split; }
+
+  auto setFromINotify(string s)
+  {
+    auto m = s.matchFirst(r"(\d+) (/.*?/(.*?)/.*)");
+    if (!m.empty) {
+      prevTime = time;
+      prevPath = path;
+      time = m[1].to!long;
+      path = m[2].to!string;
+      site = m[3].to!string;
+      valid = exists(path) && !isDir(path) && sites.canFind(site);
+      if (valid) sleep();
+    } else {
+      valid = false;
+    }
+  }
+
+  auto sleep()
+  {
+    // Some editors notify several notifications during short time
+    // and the notificated file may disappear in the moment.
+    import core.thread;
+    Thread.sleep(dur!("msecs")(200));
+  }
+
+  auto changed()
+  {
+    return path != prevPath || time - prevTime > 2;
+  }
 }
 
-auto getUrl(string path)
+struct Url
 {
-  auto file = File(path);
-  foreach (line; file.byLine) {
-    auto m = line.matchFirst(r"// URL: (.*)");
-    if (!m.empty) return m[1].to!string;
+  string url, prevUrl;
+  bool valid;
+  alias url this;
+
+  auto setFromFile(string path)
+  {
+    auto file = File(path, "r");
+    scope(exit) file.close();
+    foreach (line; file.byLine) {
+      auto m = line.matchFirst(r"// URL: (.*)");
+      if (!m.empty) {
+        prevUrl = url;
+        url = m[1].to!string;
+        valid = true;
+        return;
+      }
+    }
+    valid = false;
   }
-  return "";
+
+  auto changed() { return url != prevUrl; }
 }
 
 auto runProcess(string[] cmd, string[string] env = null)
