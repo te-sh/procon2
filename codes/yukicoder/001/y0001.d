4193d1defb069d3bac582a3fe34e0eff636efaf4
te-sh <admire.japan@gmail.com>
2020-01-18 16:16:05 +0900

    add yukicoder No.1,5

---
+++ codes/yukicoder/001/y0001.d
@@ -0,0 +1,122 @@
+// URL: https://yukicoder.me/problems/no/1
+
+import std.algorithm, std.container, std.math, std.range, std.typecons, std.string;
+
+version(unittest) {} else
+void main()
+{
+  int N; io.getV(N);
+  int C; io.getV(C);
+  int V; io.getV(V);
+  int[] S; io.getA(V, S); --S[];
+  int[] T; io.getA(V, T); --T[];
+  int[] Y; io.getA(V, Y);
+  int[] M; io.getA(V, M);
+
+  auto g = GraphW!int(N*(C+1));
+  foreach (Si, Ti, Yi, Mi; lockstep(S, T, Y, M))
+    foreach (c; 0..C-Yi+1)
+      g.addEdge((C+1)*Si+c, (C+1)*Ti+c+Yi, Mi);
+
+  auto r = g.dijkstra(0).dist[(N-1)*(C+1)..N*(C+1)].minElement;
+  io.putB(r < g.inf, r, -1);
+}
+
+struct GraphW(W = int, W i = 10^^9)
+{
+  alias Node = int, Wt = W, inf = i;
+  struct Edge { Node src, dst; Wt wt; alias cap = wt; }
+  Node n;
+  Edge[][] g;
+  alias g this;
+  this(Node n) { this.n = n; g = new Edge[][](n); }
+  void addEdge(Node u, Node v, Wt w) { g[u] ~= Edge(u, v, w); }
+  void addEdgeB(Node u, Node v, Wt w) { g[u] ~= Edge(u, v, w); g[v] ~= Edge(v, u, w); }
+}
+
+struct Dijkstra(Graph)
+{
+  import std.array, std.container;
+  Graph g;
+  alias g this;
+  alias Node = g.Node, Wt = g.Wt, Edge = g.Edge;
+  Wt[] dist;
+  Node[] prev;
+
+  this(Graph g, Node s)
+  {
+    this.g = g;
+    auto sent = n;
+
+    dist = new Wt[](n);
+    dist[] = g.inf;
+    dist[s] = 0;
+
+    prev = new Node[](n);
+    prev[] = sent;
+
+    auto q = heapify!("a.wt>b.wt")(Array!Edge(Edge(sent, s, 0)));
+    while (!q.empty) {
+      auto e = q.front; q.removeFront();
+      if (prev[e.dst] != sent) continue;
+      prev[e.dst] = e.src;
+      foreach (f; g[e.dst]) {
+        auto w = e.wt+f.wt;
+        if (dist[f.dst] > w) {
+          dist[f.dst] = w;
+          q.insert(Edge(f.src, f.dst, w));
+        }
+      }
+    }
+  }
+}
+Dijkstra!Graph dijkstra(Graph, Node)(Graph g, Node s) { return Dijkstra!Graph(g, s); }
+
+auto io = IO!()();
+
+import std.stdio;
+struct IO(alias IN = stdin, alias OUT = stdout, string delimiter = " ", string floatFormat = "%.10f")
+{
+  import std.algorithm, std.conv, std.format, std.meta, std.range, std.stdio, std.traits;
+  alias assignable = hasAssignableElements;
+
+  dchar[] buf;
+  auto sp = (new dchar[](0)).splitter;
+  void nextLine() { IN.readln(buf); sp = buf.splitter; }
+
+  auto get(T)(ref T v) { if (sp.empty) nextLine(); v = sp.front.to!T; sp.popFront(); }
+  auto getV(T...)(ref T v) { foreach (ref w; v) get(w); }
+  auto getA(T)(size_t n, ref T v) if (assignable!T) { v = new T(n); foreach (ref w; v) get(w); }
+  auto getC(T...)(size_t n, ref T v) if (allSatisfy!(assignable, T))
+  {
+    foreach (ref w; v) w = new typeof(w)(n);
+    foreach (i; 0..n) foreach (ref w; v) get(w[i]);
+  }
+  auto getM(T)(size_t r, size_t c, ref T v) if (assignable!T && assignable!(ElementType!T))
+  {
+    v = new T(r); foreach (ref w; v) getA(c, w);
+  }
+
+  auto putR(T)(T v)
+  {
+    auto w = v;
+    while (!w.empty) {
+      putA(w.front); w.popFront();
+      if (!w.empty) OUT.write(delimiter);
+    }
+  }
+  auto putA(T)(T v)
+  {
+    static if (isInputRange!T && !isSomeString!T) putR(v);
+    else if (isFloatingPoint!T) OUT.write(format(floatFormat, v));
+    else OUT.write(v);
+  }
+  auto put(T...)(T v)
+  {
+    foreach (i, w; v) { putA(w); if (i < v.length-1) OUT.write(delimiter); }
+    OUT.writeln;
+  }
+
+  auto putB(S, T)(bool c, S t, T f) { if (c) put(t); else put(f); }
+  auto putRaw(T...)(T v) { OUT.write(v); OUT.writeln; }
+}
