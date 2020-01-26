5ef670d286a235dc94bfb0e600d2ab1b8df69bc7
te-sh <admire.japan@gmail.com>
2020-01-26 14:43:38 +0900

    update yukicoder No.656,657,658,659,660

--- codes-d/yukicoder/014/y0659.d
+++ codes-d/yukicoder/014/y0659.d
@@ -1,120 +1,41 @@
 // URL: https://yukicoder.me/problems/no/659
 
-import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
-
-auto rdsp(){return readln.splitter;}
-void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
-void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
-void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
-void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
-void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
-void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
+import std.algorithm, std.array, std.container, std.math, std.range, std.typecons, std.string;
 
 version(unittest) {} else
 void main()
 {
-  int r, c, t; readV(r, c, t);
-  int sx, sy; readV(sx, sy);
-  int gx, gy; readV(gx, gy);
-  string[] b; readC(r, b);
+  int R, C, T; io.getV(R, C, T);
+  int Sx, Sy; io.getV(Sx, Sy);
+  int Gx, Gy; io.getV(Gx, Gy);
+  string[] B; io.getC(R, B);
 
-  auto n = (r-2)*(c-2), a = Matrix!real(n, n);
-  auto idx(int i, int j) { return (i-1)*(c-2)+(j-1); }
+  auto n = (R-2)*(C-2), a = Matrix!double(n, n);
+  auto idx(int i, int j) { return (i-1)*(C-2)+(j-1); }
 
-  foreach (i; 1..r-1)
-    foreach (j; 1..c-1) {
-      if (b[i][j] == '#') continue;
-      auto d = [b[i-1][j], b[i+1][j], b[i][j-1], b[i][j+1]].map!(bij => bij == '.' ? 1 : 0).sum;
+  foreach (i; 1..R-1)
+    foreach (j; 1..C-1) {
+      if (B[i][j] == '#') continue;
+      auto d = [B[i-1][j], B[i+1][j], B[i][j-1], B[i][j+1]].map!(bij => bij == '.' ? 1 : 0).sum;
       auto id1 = idx(i, j);
       if (d == 0) {
         a[id1][id1] = 1;
       } else {
-        if (b[i-1][j] == '.') a[idx(i-1, j)][id1] = 1.0L/d;
-        if (b[i+1][j] == '.') a[idx(i+1, j)][id1] = 1.0L/d;
-        if (b[i][j-1] == '.') a[idx(i, j-1)][id1] = 1.0L/d;
-        if (b[i][j+1] == '.') a[idx(i, j+1)][id1] = 1.0L/d;
+        if (B[i-1][j] == '.') a[idx(i-1, j)][id1] = 1.0/d;
+        if (B[i+1][j] == '.') a[idx(i+1, j)][id1] = 1.0/d;
+        if (B[i][j-1] == '.') a[idx(i, j-1)][id1] = 1.0/d;
+        if (B[i][j+1] == '.') a[idx(i, j+1)][id1] = 1.0/d;
       }
     }
 
-  auto v = new real[](n);
-  v[] = 0; v[idx(sx, sy)] = 1;
+  auto v = Vector!double(n);
+  v[idx(Sx, Sy)] = 1;
 
-  auto p = repeatedSquare(a, t, Matrix!real.unit(n)) * v;
-  writefln("%.7f", p[idx(gx, gy)]);
+  auto p = a^^T * v;
+  io.put(p[idx(Gx, Gy)]);
 }
 
-pure T repeatedSquare(alias pred = "a * b", T, U)(T a, U n)
-{
-  return repeatedSquare!(pred, T, U)(a, n, T(1));
-}
+import lib.math.matrix;
 
-pure T repeatedSquare(alias pred = "a * b", T, U)(T a, U n, T init)
-{
-  import std.functional;
-  alias predFun = binaryFun!pred;
-
-  if (n == 0) return init;
-
-  auto r = init;
-  while (n > 0) {
-    if (n&1) r = predFun(r, a);
-    a = predFun(a, a);
-    n >>= 1;
-  }
-
-  return r;
-}
-
-struct Matrix(T)
-{
-  size_t r, c;
-  T[][] a;
-  alias a this;
-
-  static ref auto unit(size_t n)
-  {
-    auto r = Matrix!T(n, n);
-    foreach (i; 0..n) r[i][i] = 1;
-    return r;
-  }
-
-  this(size_t r, size_t c)
-  {
-    this.r = r; this.c = c;
-    a = new T[][](r, c);
-    static if (T.init != 0) foreach (i; 0..r) a[i][] = 0;
-  }
-
-  this(T[][] b)
-  {
-    r = b.length;
-    c = b[0].length;
-    a = b;
-  }
-
-  ref auto dup() { auto x = Matrix!T(r, c); foreach (i; 0..r) x[i][] = a[i][]; return x; }
-
-  ref auto opBinary(string op)(Matrix!T b) if (op == "+" || op == "-") in { assert(r == b.r && c == b.c); } body
-  {
-    auto x = Matrix!T(r, c);
-    foreach (i; 0..r) foreach (j; 0..c) x[i][j] = mixin("a[i][j]"~op~"b[i][j]");
-    return x;
-  }
-
-  ref auto opBinary(string op: "*")(Matrix!T b) in { assert(c == b.r); } body
-  {
-    auto x = Matrix!T(r, b.c);
-    foreach (i; 0..r) foreach (j; 0..b.c) foreach (k; 0..c) x[i][j] += a[i][k]*b[k][j];
-    return x;
-  }
-
-  ref auto opBinary(string op: "*")(T[] b) in { assert(c == b.length); } body
-  {
-    auto x = new T[](r);
-    static if (T.init != 0) x[] = 0;
-    foreach (i; 0..r) foreach (j; 0..c) x[i] += a[i][j]*b[j];
-    return x;
-  }
-}
+auto io = IO!()();
+import lib.io;
--- codes-d/yukicoder/014/y0660.d
+++ codes-d/yukicoder/014/y0660.d
@@ -1,96 +1,28 @@
 // URL: https://yukicoder.me/problems/no/660
 
-import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
-
-auto rdsp(){return readln.splitter;}
-void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
-void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
-void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
-void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
-void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
-void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
-
-const mod = 10^^9+7;
-alias mint = FactorRing!mod;
+import std.algorithm, std.array, std.container, std.math, std.range, std.typecons, std.string;
 
 version(unittest) {} else
 void main()
 {
-  int n; readV(n);
-
-  auto fact = new mint[](n*2+1);
-  fact[0] = 1;
-  foreach (i; 1..n*2+1) fact[i] = fact[i-1]*i;
-  auto invFact = new mint[](n*2+1);
-  invFact[n*2] = fact[n*2].inv;
-  foreach_reverse (i; 1..n*2+1) invFact[i-1] = invFact[i]*i;
+  int N; io.getV(N);
+  
+  auto fact = Fact!mint(N*2);
 
   auto ans = mint(0);
-  for (auto k = n; k <= n*2; k += 2) {
-    auto a = (k+n)/2, b = (k-n)/2;
-    ans += fact[a+b-1]*invFact[a-1]*invFact[b];
-    ans -= fact[a+b-1]*invFact[a]*invFact[b-1];
+  for (auto k = N; k <= N*2; k += 2) {
+    auto a = (k+N)/2, b = (k-N)/2;
+    ans += fact.combi(a+b-1, b) - fact.combi(a+b-1, b-1);
   }
 
-  writeln(ans);
+  io.put(ans);
 }
 
-struct FactorRing(int m, bool pos = false)
-{
-  version(BigEndian) union { long vl; struct { int vi2; int vi; } } else union { long vl; int vi; }
-  alias FR = FactorRing!(m, pos);
-  @property static init() { return FR(0); }
-  @property int value() { return vi; }
-  @property void value(int v) { vi = mod(v); }
-  alias value this;
+import lib.math.fact;
 
-  this(int v) { vi = v; }
-  this(int v, bool runMod) { vi = runMod ? mod(v) : v; }
-  this(long v) { vi = mod(v); }
+const mod = 10^^9+7;
+alias mint = ModInt!mod;
+import lib.math.mod_int;
 
-  ref auto opAssign(int v) { vi = v; return this; }
-
-  pure auto mod(int v) const { static if (pos) return v%m; else return (v%m+m)%m; }
-  pure auto mod(long v) const { static if (pos) return cast(int)(v%m); else return cast(int)((v%m+m)%m); }
-
-  static if (!pos) pure ref auto opUnary(string op: "-")() { return FR(mod(-vi)); }
-
-  static if (m < int.max / 2) {
-    pure ref auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FR(mod(mixin("vi"~op~"r"))); }
-    ref auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vi"~op~"r")); return this; }
-  } else {
-    pure ref auto opBinary(string op)(int r) if (op == "+" || op == "-") { return FR(mod(mixin("vl"~op~"r"))); }
-    ref auto opOpAssign(string op)(int r) if (op == "+" || op == "-") { vi = mod(mixin("vl"~op~"r")); return this; }
-  }
-  pure ref auto opBinary(string op: "*")(int r) { return FR(mod(vl*r)); }
-  ref auto opOpAssign(string op: "*")(int r) { vi = mod(vl*r); return this; }
-
-  pure ref auto opBinary(string op)(ref FR r) if (op == "+" || op == "-" || op == "*") { return opBinary!op(r.vi); }
-  ref auto opOpAssign(string op)(ref FR r) if (op == "+" || op == "-" || op == "*") { return opOpAssign!op(r.vi); }
-
-  pure auto opBinary(string op: "/")(FR r) { return FR(mod(vl*r.inv.vi)); }
-  pure auto opBinary(string op: "/")(int r) { return opBinary!op(FR(r)); }
-  ref auto opOpAssign(string op: "/")(ref FR r) { vi = mod(vl*r.inv.vi); return this; }
-  ref auto opOpAssign(string op: "/")(int r) { return opOpAssign!op(FR(r)); }
-
-  pure auto inv()
-  {
-    int x = vi, a, b;
-    exEuclid(x, m, a, b);
-    return FR(mod(a));
-  }
-}
-
-pure T exEuclid(T)(T a, T b, ref T x, ref T y)
-{
-  auto g = a;
-  x = 1;
-  y = 0;
-  if (b) {
-    g = exEuclid(b, a%b, y, x);
-    y -= a/b*x;
-  }
-  return g;
-}
+auto io = IO!()();
+import lib.io;
