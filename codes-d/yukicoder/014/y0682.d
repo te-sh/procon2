0c86239de37ee58ea5a928a39accbde37b3453b5
te-sh <admire.japan@gmail.com>
2020-01-27 20:22:03 +0900

    add yukicoder No.681,682,683,684

--- codes-d/yukicoder/014/y0682.d
+++ codes-d/yukicoder/014/y0682.d
@@ -1,25 +1,18 @@
 // URL: https://yukicoder.me/problems/no/682
 
-import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
-
-auto rdsp(){return readln.splitter;}
-void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
-void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
-void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
-void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
-void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
-void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
+import std.algorithm, std.array, std.container, std.math, std.range, std.typecons, std.string;
 
 version(unittest) {} else
 void main()
 {
-  int a, b; readV(a, b);
+  int a, b; io.getV(a, b);
 
   auto ans = 0;
   foreach (i; a..b+1)
     if ((a+b+i)%3 == 0) ++ans;
 
-  writeln(ans);
+  io.put(ans);
 }
+
+auto io = IO!()();
+import lib.io;
--- codes-d/yukicoder/014/y0683.d
+++ codes-d/yukicoder/014/y0683.d
@@ -1,21 +1,11 @@
 // URL: https://yukicoder.me/problems/no/683
 
-import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
-
-auto rdsp(){return readln.splitter;}
-void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
-void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
-void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
-void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
-void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
-void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
+import std.algorithm, std.array, std.container, std.math, std.range, std.typecons, std.string;
 
 version(unittest) {} else
 void main()
 {
-  long a, b; readV(a, b);
+  long A, B; io.getV(A, B);
 
   bool[long][long] memo;
   bool calc(long a, long b)
@@ -29,5 +19,8 @@ void main()
     return memo[a][b] = false;
   }
 
-  writeln(calc(a, b) ? "Yes" : "No");
+  io.putB(calc(A, B), "Yes", "No");
 }
+
+auto io = IO!()();
+import lib.io;
