1593569f420496bdc2f1b0870985f27f1be4daee
te-sh <admire.japan@gmail.com>
2020-01-28 12:33:12 +0900

    update yukicoder NO.716,717,718,720

--- codes-d/yukicoder/015/y0717.d
+++ codes-d/yukicoder/015/y0717.d
@@ -1,23 +1,16 @@
 // URL: https://yukicoder.me/problems/no/717
 
-import std.algorithm, std.container, std.conv, std.math, std.range, std.typecons, std.stdio, std.string;
-
-auto rdsp(){return readln.splitter;}
-void pick(R,T)(ref R r,ref T t){t=r.front.to!T;r.popFront;}
-void pickV(R,T...)(ref R r,ref T t){foreach(ref v;t)pick(r,v);}
-void readV(T...)(ref T t){auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readA(T)(size_t n,ref T[]t){t=new T[](n);auto r=rdsp;foreach(ref v;t)pick(r,v);}
-void readM(T)(size_t r,size_t c,ref T[][]t){t=new T[][](r);foreach(ref v;t)readA(c,v);}
-void readC(T...)(size_t n,ref T t){foreach(ref v;t)v=new typeof(v)(n);foreach(i;0..n){auto r=rdsp;foreach(ref v;t)pick(r,v[i]);}}
-void readS(T)(size_t n,ref T t){t=new T(n);foreach(ref v;t){auto r=rdsp;foreach(ref j;v.tupleof)pick(r,j);}}
-void writeA(T)(size_t n,T t){foreach(i,v;t.enumerate){write(v);if(i<n-1)write(" ");}writeln;}
+import std.algorithm, std.array, std.container, std.math, std.range, std.typecons, std.string;
 
 version(unittest) {} else
 void main()
 {
-  int n, m; readV(n, m);
-  string s; readV(s);
-  string t; readV(t);
+  int N, M; io.getV(N, M);
+  string S; io.getV(S);
+  string T; io.getV(T);
 
-  writeln(min(s.count('A'), t.count('A')) + min(s.count('B'), t.count('B')));
+  io.put(min(S.count('A'), T.count('A')) + min(S.count('B'), T.count('B')));
 }
+
+auto io = IO!()();
+import lib.io;
