3b858114587bce5d37d6f5b12a8d2dd6187fe41c
te-sh <admire.japan@gmail.com>
2020-02-04 00:56:35 +0900

    add yukicoder No.184

---
+++ codes-d/yukicoder/004/y0184.d
@@ -0,0 +1,31 @@
+// URL: https://yukicoder.me/problems/no/184
+
+import std.algorithm, std.array, std.container, std.math, std.range, std.typecons, std.string;
+
+version(unittest) {} else
+void main()
+{
+  int N; io.getV(N);
+  long[] A; io.getA(N, A);
+
+  auto i = 0, j = 0;
+  while (i <= 60 && j < N) {
+    auto k = A[j..$].countUntil!(Ai => Ai.bitTest(i));
+    if (k == -1) {
+      ++i;
+      continue;
+    }
+    k += j;
+    swap(A[j], A[k]);
+    foreach (l; 0..N)
+      if (l != j && A[l].bitTest(i)) A[l] ^= A[j];
+    ++i; ++j;
+  }
+
+  io.put(2L^^(A.count!"a>0"));
+}
+
+import lib.bitop;
+
+auto io = IO!()();
+import lib.io;
